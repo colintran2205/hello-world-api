86487d1ca6c4556ce985dcf13ba0151da1242db4
colintran2205 <procango2003@gmail.com>
2025-08-26 09:54:43 +0700

    chore: add ESLint configuration and dependencies for code linting

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+export default defineConfig([
+  { 
+    files: ['**/*.{js}'], 
+    plugins: { js }, 
+    extends: ['js/recommended'], 
+    languageOptions: { globals: globals.browser }, 
+    rules: { // custom eslint rules
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'eol-last': ['error', 'always'],
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    } 
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+]);
